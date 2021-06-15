3c3ecf10d043905eaa95184d5799573073381a72
LAPTOP-PRH62NGH\ASUS <vinziichan0505@gmail.com>
2021-06-15 15:00:51 +0700

    fixed error database

--- index.js
+++ index.js
@@ -1,67 +1,17 @@
 const express = require('express')
-const app = express()
-const mysql = require('mysql')
 const cors = require('cors')
-const bodyparser = require('body-parser')
 
-const corsOptions ={
-    origin:'*',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const app = express()
+const routerTodos = require('./router/todo.js')
+const routerUsers = require('./router/user')
 
-const conn = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'develop'
-})
-app.use(cors(corsOptions))
-app.use(bodyparser.urlencoded())
-app.use(bodyparser.json())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
 
-conn.connect(function(err){
-    if(err)
-        throw err
-})
-app.get('/', function(req,res){
-    res.send(`
-        <form method="post" action="/todo">
-            <input Name="Name" style="margin-right:10px"/>
-            <input type="submit"/>
-        </form>
-    `)
-})
-app.post('/todo',function(req,res){
-    const sql = `INSERT INTO data (Name) VALUES (\'${req.body.Name}\')`
-    conn.query(sql,function(err){
-        if(err) 
-            throw err
-        console.log('Data Added')
-    })
-    res.end()
-})
-
-app.get('/todo',function(req,res){
-    const sql = 'SELECT * FROM data'
-    conn.query(sql,function(err,result){
-        if(err)
-            throw err
-        res.send(result)
-        console.log(result)
-    })
-})
-
-app.delete('/todo/:Name',function(req,res){
-    const query = `DELETE FROM data where Name=\'${req.params.Name}\'`
-    conn.query(query,function(err,result){
-        if(err)
-            throw err
-        res.send("Deleted !")
-    })
-})
+app.use(routerTodos)
+app.use(routerUsers)
 
 app.listen(3000, () => {
-    console.log('Server Done')
+    console.log('Server Running Port : 3000')
 })
---
+++ router/user.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const router = express.Router()
+const mysql = require('mysql')
+
+router.use(express.json())
+router.use(express.urlencoded())
+
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'develop'
+})
+
+conn.connect(function(err){
+    if(err)
+        throw err
+})
+
+router.get('/user', function (req, res) {
+    res.send(`
+        <html>
+            <form action="/user" method="POST">
+                <label>Username</label>
+                <input name = "username"></input>
+                <label>Password</label>
+                <input name = "password" type = "password"></input>
+                <button>Submit</button>
+            </form>
+        </html>
+    `)
+})
+
+router.post('/user',function(req,res){
+    const sql = `INSERT INTO users (username, password) VALUES (\'${req.body.username}\', \'${req.body.password}\')`
+    conn.query(sql,function(err){
+        if(err) 
+            throw err
+        console.log('User Added')
+    })
+    res.sendStatus(200)
+})
+
+router.get('/users',function(req,res){
+    const sql = 'SELECT * FROM users'
+    conn.query(sql,function(err,result){
+        if(err)
+            throw err
+        res.send(result)
+        console.log(result)
+    })
+})
+
+router.delete('user/:id',function(req,res){
+    const query = `DELETE FROM users WHERE id=\'${req.params.id}\'`
+    conn.query(query,function(err,result){
+        if(err)
+            throw err
+        res.send("User Deleted")
+    })
+})
+
+module.exports = router
